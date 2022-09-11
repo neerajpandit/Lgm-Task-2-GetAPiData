44995fd78f2d22c31c09713b9bcfcfb03556a7fb
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-09-11 17:31:37 +0530

    Initial

---
+++ src/component/useEffect/loading.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className="loadingDiv">
+            <br>
+
+                
+            </br>
+            <h1>Loading...</h1>
+            <br>
+
+
+
+            </br>
+        </div>
+    )
+}
+
+export default Loading
